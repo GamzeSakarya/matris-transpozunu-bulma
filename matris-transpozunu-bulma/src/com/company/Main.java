1c5f2ec600684c97a44d446f377d86f22048701b
Gamze Sakarya <126356427+GamzeSakarya@users.noreply.github.com>
2023-09-25 13:17:53 +0300

    Add files via upload

---
+++ matris-transpozunu-bulma/src/com/company/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {2, 3, 4},
+                {5, 6, 4}
+        };
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[][] transpose = new int[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        System.out.println("Matris : ");
+        printMatrix(matrix);
+
+        System.out.println("Transpoze : ");
+        printMatrix(transpose);
+    }
+
+    static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
